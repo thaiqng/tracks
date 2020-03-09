103326cb9575c86df703e2d6a2419cf432d51459
Thai Nguyen <developers@buscoro.com>
2020-03-09 17:08:50 -0600

    add token_auth and verify_token in root schema

--- tracksApp/tracksApp/schema.py
+++ tracksApp/tracksApp/schema.py
@@ -1,12 +1,16 @@
 import graphene
 import tracks.schema # to use the root query of the tracks schema
 import users.schema
+import graphene
+import graphql_jwt
 
 class Query(users.schema.Query, tracks.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(users.schema.Mutation, tracks.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field() # provide jwt token if the given credentials are correct
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 # create SCHEMA
 schema = graphene.Schema(query=Query, mutation=Mutation)
