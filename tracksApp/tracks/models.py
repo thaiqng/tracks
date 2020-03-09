064686c2233c795419a5098e2014b4e332b533d3
Thai Nguyen <developers@buscoro.com>
2020-03-09 11:06:25 -0600

    code Track model

--- tracksApp/tracks/models.py
+++ tracksApp/tracks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Track(models.Model):
+    # id field is auto added
+    title = models.CharField(maxlength=50)
+    description = models.TextField(blank=True) # optional
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True) # auto set to current DateTimeField
