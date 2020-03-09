9172c31139a0340f45a88278869994537a335637
Thai Nguyen <developers@buscoro.com>
2020-03-09 13:05:11 -0600

    code main Mutation class

--- tracksApp/tracksApp/schema.py
+++ tracksApp/tracksApp/schema.py
@@ -4,5 +4,8 @@ import tracks.schema # to use the root query of the tracks schema
 class Query(tracks.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(tracks.schema.Mutation, graphene.ObjectType):
+    pass
+
 # create SCHEMA
 schema = graphene.Schema(query=Query)
