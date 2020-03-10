54958e7772617ae51d171aab6d81de9da8c2d7c0
Thai Nguyen <developers@buscoro.com>
2020-03-10 17:20:52 -0600

    create and wrap apolloClient around root component

--- react-tracks-client/src/index.js
+++ react-tracks-client/src/index.js
@@ -2,8 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./Root";
 import * as serviceWorker from "./serviceWorker";
+import {ApolloProvider} from 'react-apollo' // to inject apollo into all components with the help of React Context
+import ApolloClient from 'apollo-boost'
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+// create apollo client
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql/'
+})
+
+// wrap Root component inside ApolloClient and pass client into client prop
+ReactDOM.render(
+  <ApolloClient client={client}>
+    <Root />
+  </ApolloClient>,
+  document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
